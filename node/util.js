3f43fa1bf3ef20c35993367f6c1ab6ab6e500ba5
ElevenNotes <github@11notes.ch>
2024-01-22 16:28:19 +0100

    elevenLogJSON add DEBUG mode

--- node/util.js
+++ node/util.js
@@ -1,9 +1,11 @@
 const elevenLogJSON = (type, message) => {
-  console.log(JSON.stringify({
-    time:new Date().toISOString(),
-    type:type.toUpperCase(),
-    msg:message
-  }));
+  if((/debug/i.test(type) && process.env?.DEBUG) || true){
+    console.log(JSON.stringify({
+      time:new Date().toISOString(),
+      type:type.toUpperCase(),
+      msg:message
+    }));
+  }
 }
 
 module.exports = {
