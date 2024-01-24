6d65b162c29d1acc0fb5617986a59677c9dff38c
ElevenNotes <github@11notes.ch>
2024-01-24 22:19:06 +0100

    Update util.js

--- node/util.js
+++ node/util.js
@@ -1,17 +1,9 @@
-const { inspect } = require('node:util');
-
 const elevenLogJSON = (type, message) => {
   if(process.env?.DEBUG || (!process.env?.DEBUG && !/debug/i.test(type))){
     console.log(JSON.stringify({
       time:new Date().toISOString(),
       type:type.toUpperCase(),
-      msg:inspect(message, {
-        showHidden:false,
-        depth:7,
-        maxStringLength:64,
-        compact:true,
-        sorted:true
-      })
+      msg:message
     }));
   }
 }
