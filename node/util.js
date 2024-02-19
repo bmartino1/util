f9212f3786a228bca17400c50255a5d6b556cda5
ElevenNotes <github@11notes.ch>
2024-02-19 11:19:31 +0100

    shorter type

--- node/util.js
+++ node/util.js
@@ -1,5 +1,12 @@
 const elevenLogJSON = (type, message) => {
   if(process.env?.DEBUG || (!process.env?.DEBUG && !/debug/i.test(type))){
+    switch(true){
+      case /DEBUG|DBG|DEB/i.test(type): type = 'DBG'; break;
+      case /INFO|INF/i.test(type): type = 'INF'; break;
+      case /WARNING|WARN|WRN/i.test(type): type = 'WRN'; break;
+      case /ERROR|ERR/i.test(type): type = 'ERR'; break;
+      default: type = 'INF';
+    }
     console.log(JSON.stringify({
       time:new Date().toISOString(),
       type:type.toUpperCase(),
